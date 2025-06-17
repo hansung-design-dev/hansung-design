906aed4f3640681a8b2d08a568812a1716831945
sung-eunji <danceunji@gmail.com>
2025-06-17 11:56:56 +0200

    landing mediaquery

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -52,33 +52,34 @@ const Section = ({
   list,
 }: SectionProps) => {
   return (
-    <section className="h-screen w-full flex items-center justify-center scroll-container sm:px-[4rem] sm:w-[90%] md:w-[90%] md:px-[2rem] ">
+    <section className="h-screen w-full flex items-center justify-center scroll-container sm:px-[0rem] sm:w-full md:w-[90%] md:px-[2rem] ">
       <motion.div
         variants={staggerContainer}
         initial="initial"
         whileInView="animate"
         viewport={{ once: false, amount: 0.2 }}
-        className={`scroll-area px-[1.25rem] sm:px-[1rem] md:px-[2rem] lg:flex flex-col ${
+        className={`scroll-area px-[1.25rem]  md:px-[2rem] lg:flex flex-col sm:px-[0rem] ${
           reverse ? 'md:flex-row-reverse' : 'md:flex-row'
         } items-start gap-[2.5rem] sm:gap-[2rem] `}
       >
         <motion.div
           variants={fadeInUp}
-          className="flex-1 w-full px-[5rem] sm:px-[2rem] md:px-[3rem] pt-[2rem] sm:pt-[1rem] md:pt-[1rem]"
+          className="flex-1 w-full px-[5rem] sm:px-[1rem] sm:w-[17rem] md:px-[3rem] pt-[2rem] sm:pt-[1rem] md:pt-[1rem]"
         >
-          <div className="flex lg:flex-col gap-[2rem] sm:gap-[1.5rem] md:gap-[2rem] max-w-xl md:pb-3 md:items-start sm:flex-col sm:pb-6">
+          <div className="flex lg:flex-col gap-[2rem]  md:gap-[2rem] max-w-xl md:pb-3 md:items-start sm:flex-col sm:pb-6 sm:gap-[2.5rem]">
             <div className="flex flex-col gap-[0.75rem] sm:gap-[0.75rem] md:gap-[1.25rem]">
               {subtitle && (
                 <motion.div
                   variants={fadeInUp}
-                  className="lg:text-2.25 sm:text-1.25 md:text-1.75 font-weight-500t"
+                  className="lg:text-2.25 sm:text-1 sm:font-weight-500 md:text-1.75 font-500"
                 >
                   {subtitle}
                 </motion.div>
               )}
+
               <motion.div
                 variants={fadeInUp}
-                className="lg:text-4.375 sm:text-2 md:text-3.5 font-bold"
+                className="lg:text-4.375  font-bold font-gmarket flex flex-col text-center sm:text-left sm:text-1.75 md:text-3.5 font-weight-700 sm:line-height-[2.25rem]"
               >
                 {title}
               </motion.div>
@@ -92,7 +93,7 @@ const Section = ({
             <motion.div variants={fadeInUp}>
               <Link
                 href={href}
-                className="bg-black text-white w-[8rem] lg:h-[1.75rem] md:h-[2rem] sm:h-[2rem] lg:py-[1.625rem] md:py-[1.5rem] sm:py-[0.75rem] lg:px-[8rem] md:px-[6rem] sm:px-[3rem] rounded-full hover:bg-gray-800 transition-colors mobile-subtitle font-bold lg:text-1.5 md:text-1.5 sm:text-1 underline-none flex justify-center items-center"
+                className="bg-black text-white w-[8rem] lg:h-[1.75rem] md:h-[2rem] sm:h-[1.7rem] sm:w-[6rem] lg:py-[1.625rem] md:py-[1.5rem] sm:py-[0.75rem] lg:px-[8rem] md:px-[6rem] sm:px-[2rem] rounded-full hover:bg-gray-800 transition-colors mobile-subtitle font-bold lg:text-1.5 md:text-1.5 sm:text-1 sm:font-weight-500 underline-none flex justify-center items-center"
               >
                 {buttonText}
               </Link>
@@ -103,13 +104,13 @@ const Section = ({
           <div className="flex flex-col space-y-6">
             <motion.div
               variants={fadeInUp}
-              className="relative w-[52rem] sm:w-[30rem] md:w-[45rem] h-64 sm:h-56 md:h-80 "
+              className="relative w-[52rem] sm:w-full md:w-[45rem] h-64 sm:h-56 md:h-80 "
             >
               <Image
                 src={imageSrc}
                 alt={imageAlt}
                 fill
-                className="object-cover"
+                className="object-cover "
               />
             </motion.div>
             <motion.div
