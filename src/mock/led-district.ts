8213f367d5d9ab6720b48fa47399eb04f9ab5b54
sung-eunji <danceunji@gmail.com>
2025-06-21 22:47:19 +0200

    type sync

--- src/mock/led-district.ts
+++ src/mock/led-district.ts
@@ -7,12 +7,14 @@ interface District {
   size: string;
   sizeOfPeople: string;
   src: string;
+  code: string;
 }
 
 const ledDistricts: District[] = [
   {
     id: 1,
     name: '전체',
+    code: 'all',
     description: '울림픽대교 남단사거리 앞 외 3건',
     count: 4,
     icon: '/images/district-icon/all.svg',
@@ -23,6 +25,7 @@ const ledDistricts: District[] = [
   {
     id: 2,
     name: '강동구',
+    code: 'gangdong',
     description: '울림픽대교 남단사거리 앞 외 3건',
     count: 4,
     icon: '/images/district-icon/gangdong-gu.png',
@@ -33,6 +36,7 @@ const ledDistricts: District[] = [
   {
     id: 3,
     name: '광진구',
+    code: 'gwangjin',
     description: '서울대입구역 앞 외 3건',
     count: 4,
     icon: '/images/district-icon/gwangjin-gu.png',
@@ -43,6 +47,7 @@ const ledDistricts: District[] = [
   {
     id: 4,
     name: '동작구',
+    code: 'donjak',
     description: '홍대입구역 앞 외 5건',
     count: 6,
     icon: '/images/district-icon/dongjak-gu.png',
@@ -53,6 +58,7 @@ const ledDistricts: District[] = [
   {
     id: 5,
     name: '동대문구',
+    code: 'dongdaemun',
     description: '울림픽대교 남단사거리 앞 외 3건',
     count: 4,
     icon: '/images/district-icon/dongdaemun-gu.png',
